e466a6eba6dc4862b6d673db5210331efc02dedd
Bin-Huang <tohuangbin@gmail.com>
2018-09-21 12:59:01 +0800

    Init

---
+++ dist/proxy.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="node" />
+import httpProxy from 'http-proxy';
+import { IncomingMessage, ServerResponse } from 'http';
+import { Context } from 'koa';
+export default function proxy(ctx: Context, option: httpProxy.ServerOptions): Promise<{
+    msg: IncomingMessage;
+    res: ServerResponse;
+}>;
---
+++ src/proxy.ts
@@ -0,0 +1,19 @@
+import httpProxy from 'http-proxy'
+import { IncomingMessage, ServerResponse } from 'http';
+import { Context } from 'koa'
+
+const proxyClient = new httpProxy()
+
+export default async function proxy(ctx: Context, option: httpProxy.ServerOptions) {
+  return new Promise<{ msg: IncomingMessage, res: ServerResponse }>((resolve, reject) => {
+    proxyClient.web(ctx.req, ctx.res, option, (error, msg, res, url) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve({msg, res})
+      }
+    })
+  })
+}
+
+module.exports = proxy
